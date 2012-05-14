1db5db9ca0c81de9f0c2261ee287931fafc275b5
nerunblogs <nerunblogs@3b34d8ae-a482-a3b8-7621-1ab93e18e7c2>
2012-05-14 18:29:23 +0000

    Fix to Prism of Light’s mobiles (RunUO 2.2 use my scripts, SVN use RunUO team scripts).

--- trunk/Distro for RunUO 2.2/Scripts/Customs/Nerun's Distro/ML/Mobiles/Prism of Light/UnfrozenMummy.cs
+++ trunk/Distro for RunUO 2.2/Scripts/Customs/Nerun's Distro/ML/Mobiles/Prism of Light/UnfrozenMummy.cs
@@ -4,21 +4,20 @@ using Server.Items;
 
 namespace Server.Mobiles
 {
-	[CorpseName( "an unfrozen mummy corpse" )]
+	[CorpseName( "a mummy corpse" )]
 	public class UnfrozenMummy : BaseCreature
 	{
 		[Constructable]
-		public UnfrozenMummy()
-			: base( AIType.AI_Mage, FightMode.Closest, 10, 1, 0.4, 0.8 )
+		public UnfrozenMummy() : base( AIType.AI_Mage, FightMode.Closest, 10, 1, 0.4, 0.8 )
 		{
 			Name = "an unfrozen mummy";
-			Body = 0x9B;
+			Body = 0x9A;
 			Hue = 0x480;
 			BaseSoundID = 0x1D7;
 
-			SetStr( 450, 500 );
-			SetDex( 200, 250 );
-			SetInt( 800, 850 );
+			SetStr( 498 );
+			SetDex( 211 );
+			SetInt( 849 );
 
 			SetHits( 1500 );
 
@@ -28,63 +27,53 @@ namespace Server.Mobiles
 			SetDamageType( ResistanceType.Energy, 50 );
 			SetDamageType( ResistanceType.Cold, 50 );
 
-			SetResistance( ResistanceType.Physical, 35, 40 );
-			SetResistance( ResistanceType.Fire, 20, 30 );
-			SetResistance( ResistanceType.Cold, 60, 80 );
+			SetResistance( ResistanceType.Physical, 36, 40 );
+			SetResistance( ResistanceType.Fire, 22, 29 );
+			SetResistance( ResistanceType.Cold, 60, 78 );
 			SetResistance( ResistanceType.Poison, 20, 30 );
-			SetResistance( ResistanceType.Energy, 70, 80 );
+			SetResistance( ResistanceType.Energy, 69, 78 );
 
-			SetSkill( SkillName.Wrestling, 90.0, 100.0 );
+			SetSkill( SkillName.Wrestling, 90.7, 96.0 );
 			SetSkill( SkillName.Tactics, 100.0 );
 			SetSkill( SkillName.MagicResist, 250.0 );
-			SetSkill( SkillName.Magery, 50.0, 60.0 );
-			SetSkill( SkillName.EvalInt, 50.0, 60.0 );
-			SetSkill( SkillName.Meditation, 80.0 );
-
-			Fame = 25000;
-			Karma = -25000;
-
-			PackArcaneScroll( 0, 2 );
+			SetSkill( SkillName.Magery, 51.0, 57.3 );
+			SetSkill( SkillName.EvalInt, 52.8, 59.5 );
+			SetSkill( SkillName.Meditation, 80.1, 80.8 );
 		}
 
 		public override void GenerateLoot()
 		{
-			AddLoot( LootPack.UltraRich, 2 );
-			// TODO: uncomment once added
-			//AddLoot( LootPack.Parrot );
+			AddLoot( LootPack.AosUltraRich, 3 );
 		}
-
-		/*
-		// TODO: uncomment once added
+		
 		public override void OnDeath( Container c )
 		{
-			base.OnDeath( c );
-
+			base.OnDeath( c );		
+/*
 			if ( Utility.RandomDouble() < 0.6 )
 				c.DropItem( new BrokenCrystals() );
 
-			if ( Utility.RandomDouble() < 0.1 )
+			if ( Utility.RandomDouble() < 0.1 )				
 				c.DropItem( new ParrotItem() );
+*/
 		}
-		*/
 
-		public UnfrozenMummy( Serial serial )
-			: base( serial )
+		public UnfrozenMummy( Serial serial ) : base( serial )
 		{
 		}
 
 		public override void Serialize( GenericWriter writer )
 		{
 			base.Serialize( writer );
-
+			
 			writer.Write( (int) 0 ); // version
 		}
 
 		public override void Deserialize( GenericReader reader )
 		{
 			base.Deserialize( reader );
-
+			
 			int version = reader.ReadInt();
 		}
 	}
-}
+}	
