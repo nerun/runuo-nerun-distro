fa93655ab40b7a1b7c037922dff3890cfade6f5b
nerunblogs <nerunblogs@3b34d8ae-a482-a3b8-7621-1ab93e18e7c2>
2012-05-14 18:12:29 +0000

    Updated RunUO SVN support to SVN 879; Updated PremiumSpawner engine (again) to fix SeaCreatures spawning over land; Added RunUO SVN official Prism of Light’s mobiles.

--- trunk/Distro for RunUO 2.2/Scripts/Customs/Nerun's Distro/ML/Mobiles/Prism of Light/CrystalLatticeSeeker.cs
+++ trunk/Distro for RunUO 2.2/Scripts/Customs/Nerun's Distro/ML/Mobiles/Prism of Light/CrystalLatticeSeeker.cs
@@ -1,25 +1,25 @@
 using System;
-using System.Collections;
 using Server;
 using Server.Items;
 
-namespace Server.Mobiles 
-{ 
-	[CorpseName( "an crystal lattice seeker corpse" )] 
-	public class CrystalLatticeSeeker : BaseCreature 
-	{ 
-		[Constructable] 
-		public CrystalLatticeSeeker() : base( AIType.AI_Mage, FightMode.Closest, 10, 1, 0.2, 0.4 ) 
-		{		
-			Name = "a crystal lattice seeker";
+namespace Server.Mobiles
+{
+	[CorpseName( "a Crystal Lattice Seeker corpse" )]
+	public class CrystalLatticeSeeker : BaseCreature
+	{
+		[Constructable]
+		public CrystalLatticeSeeker()
+			: base( AIType.AI_Mage, FightMode.Closest, 10, 1, 0.2, 0.4 )
+		{
+			Name = "Crystal Lattice Seeker";
 			Body = 0x7B;
 			Hue = 0x47E;
-			
-			SetStr( 609, 843 );
-			SetDex( 191, 243 );
-			SetInt( 351, 458 );
 
-			SetHits( 358, 527 );
+			SetStr( 550, 850 );
+			SetDex( 190, 250 );
+			SetInt( 350, 450 );
+
+			SetHits( 350, 550 );
 
 			SetDamage( 13, 19 );
 
@@ -31,109 +31,123 @@ namespace Server.Mobiles
 			SetResistance( ResistanceType.Poison, 40, 50 );
 			SetResistance( ResistanceType.Energy, 40, 50 );
 
-			SetSkill( SkillName.Anatomy, 51.1, 74.2 );
-			SetSkill( SkillName.EvalInt, 90.3, 99.8 );
-			SetSkill( SkillName.Magery, 99.1, 100.0 );
-			SetSkill( SkillName.Meditation, 90.1, 99.6 );
-			SetSkill( SkillName.MagicResist, 90.6, 99.5 );
-			SetSkill( SkillName.Tactics, 90.1, 99.5 );
-			SetSkill( SkillName.Wrestling, 97.7, 100.0 );
-			
-			PackGem();
+			SetSkill( SkillName.Anatomy, 50.0, 75.0 );
+			SetSkill( SkillName.EvalInt, 90.0, 100.0 );
+			SetSkill( SkillName.Magery, 100.0, 100.0 );
+			SetSkill( SkillName.Meditation, 90.0, 100.0 );
+			SetSkill( SkillName.MagicResist, 90.0, 100.0 );
+			SetSkill( SkillName.Tactics, 90.0, 100.0 );
+			SetSkill( SkillName.Wrestling, 90.0, 100.0 );
+
+			Fame = 17000;
+			Karma = -17000;
+
+			PackArcaneScroll( 0, 2 );
 		}
-		
+
 		public override void GenerateLoot()
 		{
-			AddLoot( LootPack.AosFilthyRich, 3 );
+			AddLoot( LootPack.FilthyRich, 4 );
+			// TODO: uncomment once added
+			//AddLoot( LootPack.Parrot );
+			AddLoot( LootPack.Gems );
 		}
 
 		public override void OnGaveMeleeAttack( Mobile defender )
 		{
 			base.OnGaveMeleeAttack( defender );
 
-			if ( 0.1 >= Utility.RandomDouble() )
-				DrainLife();
+			if ( Utility.RandomDouble() < 0.1 )
+				Drain( defender );
 		}
 
 		public override void OnGotMeleeAttack( Mobile attacker )
 		{
 			base.OnGotMeleeAttack( attacker );
 
-			if ( 0.1 >= Utility.RandomDouble() )
-				DrainLife();
+			if ( Utility.RandomDouble() < 0.1 )
+				Drain( attacker );
 		}
-		
+
+		public virtual void Drain( Mobile m )
+		{
+			int toDrain;
+
+			switch ( Utility.Random( 3 ) )
+			{
+				case 0:
+				{
+					Say( 1042156 ); // I can grant life, and I can sap it as easily.
+					PlaySound( 0x1E6 );
+
+					toDrain = Utility.RandomMinMax( 3, 6 );
+					Hits += toDrain;
+					m.Hits -= toDrain;
+					break;
+				}
+				case 1:
+				{
+					Say( 1042157 ); // You'll go nowhere, unless I deem it should be so.
+					PlaySound( 0x1DF );
+
+					toDrain = Utility.RandomMinMax( 10, 25 );
+					Stam += toDrain;
+					m.Stam -= toDrain;
+					break;
+				}
+				case 2:
+				{
+					Say( 1042155 ); // Your power is mine to use as I will.
+					PlaySound( 0x1F8 );
+
+					toDrain = Utility.RandomMinMax( 15, 25 );
+					Mana += toDrain;
+					m.Mana -= toDrain;
+					break;
+				}
+			}
+		}
+
+		/*
+		// TODO: uncomment once added
 		public override void OnDeath( Container c )
 		{
 			base.OnDeath( c );
-/*
-			if ( Utility.RandomDouble() < 0.3 )
+
+			if ( Utility.RandomDouble() < 0.75 )
 				c.DropItem( new CrystallineFragments() );
-				
-			if ( Utility.RandomDouble() < 0.1 )
+
+			if ( Utility.RandomDouble() < 0.07 )
 				c.DropItem( new PiecesOfCrystal() );
-
-			if ( Utility.RandomDouble() < 0.1 )				
-				c.DropItem( new ParrotItem() );
-*/
 		}
-		
-		public override int Feathers{ get{ return 100; } }		
-		public override int TreasureMapLevel{ get{ return 5; } }
+		*/
 
-		public override int GetAttackSound() { return Utility.Random( 0x2F5, 2 ); }
-		public override int GetDeathSound()	{ return 0x2F7;	}
+		public override int Feathers { get { return 100; } }
+		public override int TreasureMapLevel { get { return 5; } }
+
+		public override int GetAttackSound() { return 0x2F6; }
+		public override int GetDeathSound() { return 0x2F7; }
 		public override int GetAngerSound() { return 0x2F8; }
 		public override int GetHurtSound() { return 0x2F9; }
-		public override int GetIdleSound() { return 0x2FA; }	
-				
-		public CrystalLatticeSeeker( Serial serial ) : base( serial ) 
-		{ 
-		} 
+		public override int GetIdleSound() { return 0x2FA; }
 
-		public virtual void DrainLife()
+		public CrystalLatticeSeeker( Serial serial )
+			: base( serial )
 		{
-			ArrayList list = new ArrayList();
-
-			foreach ( Mobile m in this.GetMobilesInRange( 2 ) )
-			{
-				if ( m == this || !CanBeHarmful( m ) )
-					continue;
-
-				if ( m is BaseCreature && (((BaseCreature)m).Controlled || ((BaseCreature)m).Summoned || ((BaseCreature)m).Team != this.Team) )
-					list.Add( m );
-				else if ( m.Player )
-					list.Add( m );
-			}
-
-			foreach ( Mobile m in list )
-			{
-				DoHarmful( m );
-
-				m.FixedParticles( 0x374A, 10, 15, 5013, 0x496, 0, EffectLayer.Waist );
-				m.PlaySound( 0x231 );
-
-				m.SendMessage( "You feel the life drain out of you!" );
-
-				int toDrain = Utility.RandomMinMax( 10, 40 );
-
-				Hits += toDrain;
-				m.Damage( toDrain, this );
-			}
 		}
-		
+
 		public override void Serialize( GenericWriter writer )
 		{
 			base.Serialize( writer );
-			
+
 			writer.Write( (int) 0 ); // version
 		}
 
 		public override void Deserialize( GenericReader reader )
 		{
 			base.Deserialize( reader );
-			
+
 			int version = reader.ReadInt();
 		}
 	}
-}	
+}
